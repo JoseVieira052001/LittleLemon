f7e669652b18aa6615fd58ea1dec3cec5f4ad52c
Jose Vieira <jose.vieira@nomadtech.pt>
2025-02-10 16:20:39 +0000

    Add token auth

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin 
 from django.urls import path 
 from .views import *
-  
+from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [ 
     path('', index, name='index'),
     path('menu-items/', MenuItemsView.as_view()),
     path('menu-items/<int:pk>', SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token, name='api-token-auth'),
 ]
